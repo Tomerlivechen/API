be975a6fe2a82598b281cf6d3ca5a664a61bb190
Dr. Tomer Chen <104824283+Tomerlivechen@users.noreply.github.com>
2024-08-29 17:36:25 +0300

    Lesson 7  homework

---
+++ Lesson7_HW/Models/Role.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lesson7_HW.Models
+{
+    public class Role
+    {
+        [Key] 
+        public int Id { get; set; }
+
+        [Required]
+        public required string Name { get; set; }
+    }
+}
